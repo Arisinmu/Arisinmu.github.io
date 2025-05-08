739b2a5d2fbbd2fcf0251ba1deec835e8ad5b010
Arisinmu <44214270+Arisinmu@users.noreply.github.com>
2025-05-08 21:51:52 +0900

    Update index.js

--- eitango/index.js
+++ eitango/index.js
@@ -1,41 +1,37 @@
-
 const dom_ls = {};
 dom_ls.main = document.getElementById("main");
 
+import { toMemorizeWord } from "./js/toMemorizeWord.js";
 
-let stage_ls = {
-	home: [[],[]],
-	tango: [["card","card_child"],[]]
-
-
-}
-function stage(xx){
-	for(let e in dom_ls) {
-		dom_ls[e].classList.add("kesu");
-  }
-	dom_ls.main.classList.remove("kesu");
-	for(let i = 0;i<stage_ls[xx][0].length;i++){
-		dom_ls[stage_ls[xx][0][i]].classList.remove("kesu");
-	}
-}
-
-let dbName = 'eitango';
-let storeName  = 'db1';
-var openReq = indexedDB.open(dbName, 1);
-let missdata = {
-	eitango1 : []
-};
-
-const samplecsv = {
-	name:"eitango1",
-	main:[["apple","りんご"],["banana","バナナ"],["cat","猫"],["dog","犬"],["egg","卵"],["fish","魚"],["hotel","ホテル"],["ice","アイス"],["japan","日本"],["king","王"]]
-};
-/*
-[name,[[表,裏],[]...]]
-
-*/
-	
 import { csvData } from "./js/operateCssdata.js";
+import { MenuDom } from "./js/menuDom.js"
+
+
+
+
+const app_list = {
+	menu: new MenuDom(dom_ls.main,"menu"),
+	app1 : new toMemorizeWord(dom_ls.main)
+}
+app_list.menu.createMenu(5);
+dom_ls.begininput = document.createElement("input");
+dom_ls.begininput.setAttribute("type","number");
+dom_ls.main.appendChild(dom_ls.begininput);
+dom_ls.endinput = document.createElement("input");
+dom_ls.endinput.setAttribute("type","number");
+dom_ls.main.appendChild(dom_ls.endinput);
+dom_ls.randinput = document.createElement("input");
+dom_ls.randinput.setAttribute("type","checkbox");
+dom_ls.main.appendChild(dom_ls.randinput);
+dom_ls.buttoninput = document.createElement("input");
+dom_ls.buttoninput.setAttribute("type","button");
+dom_ls.main.appendChild(dom_ls.buttoninput);
+dom_ls.buttoninput.addEventListener("click",() =>{
+
+	requireCsv.csv_data("./csv/highschool1.csv",letstart);
+
+})
+
 let highschool1Word = {
 	name:"高校1年",
 	main:[]
@@ -43,18 +39,21 @@ let highschool1Word = {
 const requireCsv = new csvData;
 function letstart(list){
 	highschool1Word.main = list;
-	app1.option.wordList = highschool1Word.main;
-	app1.option.beginNumber = prompt("最初の番号");
-  app1.option.endNumber = prompt("最後の番号");
-	app1.option.randomOrNot = confirm("ランダムですか？");
-	app1.start();
-}
-requireCsv.csv_data("./csv/highschool1.csv",letstart);
+	app_list.app1.option.wordList = highschool1Word.main;
+	app_list.app1.option.beginNumber = dom_ls.begininput.value;
+  app_list.app1.option.endNumber = dom_ls.endinput.value;
+	app_list.app1.option.randomOrNot = dom_ls.randinput.checked;
 
-import { toMemorizeWord } from "./js/toMemorizeWord.js";
-const app1 = new toMemorizeWord(dom_ls.main);
-app1.option.wordList = highschool1Word.main;
-app1.option.wordListName = highschool1Word.name;
+	dom_ls.begininput.classList.add("kesu");
+	dom_ls.endinput.classList.add("kesu");
+	dom_ls.randinput.classList.add("kesu");
+	dom_ls.buttoninput.classList.add("kesu");
+
+	app_list.app1.start();
+}
+
+app_list.app1.option.wordList = highschool1Word.main;
+app_list.app1.option.wordListName = highschool1Word.name;
 
 
 /*
